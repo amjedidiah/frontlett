f5b038af8638bfe32819cc3cc673448b4d5aaed8
amjedidiah <imunacode@gmail.com>
2020-08-16 12:01:41 +0100

    Updates

---
+++ precache-manifest.166a7414ae785607530dc5eb784e693c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af7086c87a482342bcab03c1335e3d3",
+    "url": "/frontlett/index.html"
+  },
+  {
+    "revision": "7cbbb66c84661ebdcc66",
+    "url": "/frontlett/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fc4017feab2581901cdb",
+    "url": "/frontlett/static/css/main.46b208a3.chunk.css"
+  },
+  {
+    "revision": "7cbbb66c84661ebdcc66",
+    "url": "/frontlett/static/js/2.c871ea6f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/frontlett/static/js/2.c871ea6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4017feab2581901cdb",
+    "url": "/frontlett/static/js/main.ef639ba1.chunk.js"
+  },
+  {
+    "revision": "b50cd906e5c259e670b2",
+    "url": "/frontlett/static/js/runtime-main.3bbb31b1.js"
+  },
+  {
+    "revision": "21c1bcca7c0a9eb6230c069334f934c1",
+    "url": "/frontlett/static/media/div.21c1bcca.png"
+  },
+  {
+    "revision": "9ad764182b143f4b8d58c3eb5469db96",
+    "url": "/frontlett/static/media/jumbo.9ad76418.png"
+  },
+  {
+    "revision": "f6637a611379003fee77f6da8ea11120",
+    "url": "/frontlett/static/media/logo.f6637a61.png"
+  }
+]);
